7d2d5e847191ff1ef31ec094e36be90219c026b9
andraaraya <andraelky@gmail.com>
2022-06-28 18:30:00 -0600

    a

---
+++ src/main/java/com/examenA/examenA/ExamenAApplication.java
@@ -0,0 +1,13 @@
+package com.examenA.examenA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examenA/examenA/ExamenAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examenA.examenA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
